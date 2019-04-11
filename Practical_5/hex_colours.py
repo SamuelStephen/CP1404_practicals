e0af64a064651bb3fb3fb886bdee2899390570d1
SamuelStephen <samuel.stephen@my.jcu.edu.au>
2019-04-11 13:50:15 +1000

    Changed formating

--- Practical_5/hex_colours.py
+++ Practical_5/hex_colours.py
@@ -1,4 +1,4 @@
-COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+colour_codes = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
                 "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
                 "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
                 "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
@@ -9,5 +9,5 @@ COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
 colour_name = input("Enter a colour name: ")
 while colour_name != "":
     print("The code for \"{}\" is {}".format(colour_name,
-                                             COLOUR_CODES.get(colour_name)))
+                                             colour_codes.get(colour_name)))
     colour_name = input("Enter a colour name: ")
